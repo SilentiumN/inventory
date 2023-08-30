074776a4d2be2324349093b2dcb49e87cbb7c2f0
Anastasiia Morozova <naskalelk@gmail.com>
2023-08-30 21:18:21 +0400

    confing eslint, prettier and add file for inventory

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,16 +3,27 @@ module.exports = {
   env: {
     node: true,
   },
-  extends: [
-    'plugin:vue/vue3-essential',
-    '@vue/airbnb',
-    '@vue/typescript/recommended',
-  ],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['@', './src'],
+        ],
+      },
+    },
+  },
+  extends: ['plugin:vue/vue3-essential', '@vue/airbnb', '@vue/typescript/recommended'],
   parserOptions: {
     ecmaVersion: 2020,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    indent: ['error', 2],
+    'quote-props': ['error', 'as-needed'],
+    'import/no-unresolved': ['error', { commonjs: true }],
+    'import/no-extraneous-dependencies': 'error',
+    'node/no-missing-require': 'off',
+    'node/no-extraneous-import': 'off',
   },
 };
---
+++ src/components/InventoryBlock.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="inventory" />
+</template>
+
+<style scoped lang="scss">
+  .inventory {
+    width: 29%;
+  }
+</style>
